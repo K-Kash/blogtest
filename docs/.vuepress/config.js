1241a4e0f692b6a6018f484881127949dc4b653d
Kei Kashiwadate <kei.kashiwadate@chaild.jp>
2022-05-07 16:37:12 +0900

    1

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,62 @@
+module.exports = {
+  title: '　Kash-Kiku',
+  base: '/Kash-Kiku/',
+  description: 'Blog example for Mediumish theme with VuePress',
+  logo: './assets/img/logo.png',
+  head: [["link", { rel: "icon", href: "/favicon.ico" }]],
+  theme: require.resolve('../../'),
+  themeConfig: {
+    footer: {
+      contact: [
+        {
+          type: 'codepen',
+          link: '#',
+        },
+        {
+          type: 'facebook',
+          link: '#',
+        },
+        {
+          type: 'github',
+          link: '',
+        },
+        {
+          type: 'gitlab',
+          link: '#',
+        },
+        {
+          type: 'instagram',
+          link: '#',
+        },
+        {
+          type: 'linkedin',
+          link: '#',
+        },
+        {
+          type: 'mail',
+          link: '#',
+        },
+        {
+          type: 'messenger',
+          link: '#',
+        },
+        {
+          type: 'phone',
+          link: '#',
+        },
+        {
+          type: 'twitter',
+          link: 'https://twitter.com/wowthemesnet',
+        },
+        {
+          type: 'web',
+          link: '#',
+        }
+      ],
+      copyright: [
+      ],
+    },
+
+    smoothScroll: true
+  },
+}
