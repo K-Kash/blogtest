8ea33f241a2d3192dd75fb143e9dee2c4a6adfa4
K-Kash <84908482+K-Kash@users.noreply.github.com>
2022-05-07 19:42:32 +0900

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -10,7 +10,7 @@ npm run docs:build
 cd docs/.vuepress/dist
 
 # if you are deploying to a custom domain
-# echo 'www.example.com' > CNAME
+# echo 'https://symphonious-dango-465b7b.netlify.app/' > CNAME
 
 git init
 git add -A
